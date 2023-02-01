25bb5d8420f4d681c972a671c0b60d32900ec6c2
Bruno Gritti <glbrunogl@gmail.com>
2023-02-01 10:11:33 -0300

    full package

---
+++ vue-laravel-pagination/dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueLaravelPagination from '@/vue-laravel-pagination.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueLaravelPagination
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-laravel-pagination />
+  </div>
+</template>
---
+++ vue-laravel-pagination/src/vue-laravel-pagination.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="btn-paginate">
+    <small> Mostrando {{from}} - {{to}} de {{total}} encontrados.</small>
+    <button class="btn" :disabled="from != 1"
+        @click="prevPage">&laquo; Anterior
+    </button>
+    <button class="btn" :disabled="to != total"
+        @click="nextPage">Próximo &raquo;
+    </button>
+</div>
+</template>
+
+<script>
+export default {
+    props:['from', 'to', 'total', 'perPage'],
+    data() {
+      	return {
+            limit: this.perPage || 10,
+            page: 1
+      	};
+    },
+
+    methods: {
+        prevPage: function(){
+            this.page--;
+            this.$emit('previous');
+        },
+
+        nextPage: function(){
+            this.page++;
+            this.$emit('next');
+        },
+        
+        setPage: function(page){
+            this.page = page;
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
